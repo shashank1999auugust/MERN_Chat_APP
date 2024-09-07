123636b2ab8ba7bfdca9cb6c99d268d7b7a1bb85
shashank1999auugust <sabharwalshashank@gmail.com>
2024-09-07 15:45:56 +0530

    created ui for Signup , In this we have used tailwind +daisyui ,setup was done in last commit

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -1,11 +1,76 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Signup = () => {
   return (
-    <div className='text-red-500'>
-     signup
-    </div>
-  )
-}
+    <div className="min-w-96 mx-auto">
+      {/* //use glassmorphism tailwindwebsite to generate blur medium */}
+      <div className="w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-10 border border-gray-100 ">
+        <h1 className="text-3xl font-bold text-center ">Signup</h1>
+        <form>
+          <div>
+            <label className="label p-2">
+              <span className="text-base label-text">Full Name</span>
+            </label>
+            <input
+              className="w-full input input-bordered h-10"
+              type="text"
+              placeholder="Fullname"
+            />
+          </div>
+          <div>
+            <label className="label p-2">
+              <span className="text-base label-text">Username</span>
+            </label>
+            <input
+              className="w-full input input-bordered h-10"
+              type="text"
+              placeholder="Username"
+            />
+          </div>
+          <div>
+            <label className="label p-2">
+              <span className="text-base label-text">Password</span>
+            </label>
+            <input
+              className="w-full input input-bordered h-10"
+              type="password"
+              placeholder="password"
+            />
+          </div>
+          <div>
+            <label className="label p-2">
+              <span className="text-base label-text">Confirm Password</span>
+            </label>
+            <input
+              className="w-full input input-bordered h-10"
+              type="password"
+              placeholder="password"
+            />
+          </div>
+          <div className="flex items-center my-4">
+            <div className="flex items-center">
+              <p>Male</p>
+              <input type="checkbox" defaultChecked className="checkbox mx-2" />
+            </div>
 
-export default Signup
+            <div className="flex items-center">
+              <p>Female</p>
+              <input type="checkbox" defaultChecked className="checkbox mx-2" />
+            </div>
+          </div>
+          
+            <p className="text-center">Already have an account? <Link to="/login">Signin</Link> </p>
+          
+          <div>
+            <buttton className="btn btn-block btn-sm mt-2 border-slate-700">
+              Signup
+            </buttton>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Signup;
