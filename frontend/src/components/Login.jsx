5a4059edb07bb9a33a56f467d308b7f2462f8957
shashank1999auugust <sabharwalshashank@gmail.com>
2024-09-08 13:39:38 +0530

    completed ui for login and signup , user able input the data  and its coming in console only thing left is api call

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,11 +1,69 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-  return (
-    <div>
-      login
-    </div>
-  )
-}
 
-export default Login
+    const[user,setUser]=useState({
+        username:"",
+        password:"",
+    })
+
+
+    const onSubmitHandler=(e)=>{
+      e.preventDefault()
+      console.log(user)
+      setUser({
+        username:"",
+        password:"",
+      })
+    }
+
+
+
+  return (
+    <div className="min-w-96 mx-auto">
+      {/* //use glassmorphism tailwindwebsite to generate blur medium */}
+      <div className="w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-10 border border-gray-100 ">
+        <h1 className="text-3xl font-bold text-center ">Login</h1>
+        <form onSubmit={onSubmitHandler}>
+          
+          <div>
+            <label className="label p-2">
+              <span className="text-base label-text">Username</span>
+            </label>
+            <input
+            value={user.username}
+            onChange={(e)=>setUser({...user,username:e.target.value})}
+              className="w-full input input-bordered h-10"
+              type="text"
+              placeholder="Username"
+            />
+          </div>
+          <div>
+            <label className="label p-2">
+              <span className="text-base label-text">Password</span>
+            </label>
+            <input
+             value={user.password}
+             onChange={(e)=>setUser({...user,password:e.target.value})}
+              className="w-full input input-bordered h-10"
+              type="password"
+              placeholder="password"
+            />
+          </div>
+         
+          
+            <p className="text-center my-2">Don't have an account? <Link to="/register">Signup</Link> </p>
+          
+          <div>
+            <button type="submit" className="btn btn-block btn-sm mt-2 border-slate-700">
+              Login
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -1,18 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Signup = () => {
+    const[user,setUser]=useState({
+        fullName:"",
+        username:"",
+        password:"",
+        confirmPassword:"",
+        gender:""
+    })
+
+    const handleCheckbox=(gender)=>{
+       setUser({...user, gender})
+    }
+
+
+    const onSubmitHandler=(e)=>{
+      e.preventDefault()
+      console.log(user)
+      setUser({
+        fullName:"",
+        username:"",
+        password:"",
+        confirmPassword:"",
+        gender:""
+      })
+    }
+
   return (
     <div className="min-w-96 mx-auto">
       {/* //use glassmorphism tailwindwebsite to generate blur medium */}
       <div className="w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-10 border border-gray-100 ">
         <h1 className="text-3xl font-bold text-center ">Signup</h1>
-        <form>
+        <form onSubmit={onSubmitHandler}>
           <div>
             <label className="label p-2">
               <span className="text-base label-text">Full Name</span>
             </label>
             <input
+              value={user.fullName}
+              onChange={(e)=>setUser({...user,fullName:e.target.value})}
               className="w-full input input-bordered h-10"
               type="text"
               placeholder="Fullname"
@@ -23,6 +50,8 @@ const Signup = () => {
               <span className="text-base label-text">Username</span>
             </label>
             <input
+              value={user.username}
+              onChange={(e)=>setUser({...user,username:e.target.value})}
               className="w-full input input-bordered h-10"
               type="text"
               placeholder="Username"
@@ -33,6 +62,8 @@ const Signup = () => {
               <span className="text-base label-text">Password</span>
             </label>
             <input
+              value={user.password}
+              onChange={(e)=>setUser({...user,password:e.target.value})}
               className="w-full input input-bordered h-10"
               type="password"
               placeholder="password"
@@ -43,29 +74,37 @@ const Signup = () => {
               <span className="text-base label-text">Confirm Password</span>
             </label>
             <input
+              value={user.confirmPassword}
+              onChange={(e)=>setUser({...user,confirmPassword:e.target.value})}
               className="w-full input input-bordered h-10"
               type="password"
-              placeholder="password"
+              placeholder="confirm password"
             />
           </div>
           <div className="flex items-center my-4">
             <div className="flex items-center">
               <p>Male</p>
-              <input type="checkbox" defaultChecked className="checkbox mx-2" />
+              <input type="checkbox"
+               checked={user.gender==="male"}
+                onChange={()=>handleCheckbox("male")}
+                defaultChecked className="checkbox mx-2" />
             </div>
 
             <div className="flex items-center">
               <p>Female</p>
-              <input type="checkbox" defaultChecked className="checkbox mx-2" />
+              <input type="checkbox"  
+              checked={user.gender==="female"}
+              onChange={()=>handleCheckbox("female")}
+              defaultChecked className="checkbox mx-2" />
             </div>
           </div>
           
-            <p className="text-center">Already have an account? <Link to="/login">Signin</Link> </p>
+            <p className="text-center my-2">Already have an account? <Link to="/login">Login</Link> </p>
           
           <div>
-            <buttton className="btn btn-block btn-sm mt-2 border-slate-700">
+            <button type="submit" className="btn btn-block btn-sm mt-2 border-slate-700">
               Signup
-            </buttton>
+            </button>
           </div>
         </form>
       </div>
