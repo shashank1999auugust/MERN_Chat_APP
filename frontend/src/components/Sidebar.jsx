60bfeab3057aabb7f06612820976e290446ec3a0
shashank1999auugust <sabharwalshashank@gmail.com>
2024-09-11 13:37:25 +0530

    Logout api call

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
 import { BiSearchAlt2 } from "react-icons/bi";
 import OtherUsers from './OtherUsers';
+import axios from 'axios';
+import toast from "react-hot-toast"
+import {useNavigate} from "react-router-dom"
 const Sidebar = () => {
+   const navigate= useNavigate()
+  const logoutHandler= async()=>{
+
+    try {
+       const res= await axios.get(`http://localhost:8080/api/v1/user/logout`)
+       navigate("/login")
+       toast.success(res.data.message)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+
   return (
     <div className='border-r border-slate-500 p-4 flex flex-col'>
      <form className='flex items-center gap-2'>
@@ -13,7 +29,7 @@ const Sidebar = () => {
      <div className="divider px-3"></div>
      <OtherUsers/>
      <div className='mt-2'>
-        <button className='btn btn-sm'>Logout</button>
+        <button onClick={logoutHandler} className='btn btn-sm'>Logout</button>
      </div>
     </div>
   )
