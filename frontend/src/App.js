fe90086887ea7e46054f82ba603fc0a9720957e7
shashank1999auugust <sabharwalshashank@gmail.com>
2024-09-14 12:45:44 +0530

    Socket.io for real time communication, in backend console we can see user connected with his socket id

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,9 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Signup from './components/Signup';
 import HomePage from './components/HomePage';
 import Login from './components/Login';
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import io from "socket.io-client"
 
 const router=createBrowserRouter([
   {
@@ -22,6 +25,16 @@ const router=createBrowserRouter([
 ])
 
 function App() {
+  const [socket,setSocket]= useState(null)
+  const {authUser}=useSelector(store=>store.user)
+  useEffect(()=>{
+     if(authUser){
+      const socket= io('http://localhost:8080',{
+
+      })
+      setSocket(socket)
+     }
+  },[authUser])
   return (
     <div className="p-4 h-screen flex items-center justify-center">
    <RouterProvider router={router}/>
